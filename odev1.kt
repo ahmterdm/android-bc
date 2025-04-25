744cf1a75ce0ca8499b8ba334fbafa97d0fb185f
Ahmet Erdem <121087570+ahmterdm@users.noreply.github.com>
2025-04-25 08:16:40 +0300

    Add files via upload

--- odev1.kt
+++ odev1.kt
@@ -3,51 +3,97 @@ package com.ahmeterdem.bootcampedu.degiskenler
 fun main(){
 
     var sehir = "Istanbul"
+    println("Sehir: $sehir")
     var ulke = "Türkiye"
+    println("Ulke: $ulke")
     var telefon = "0532 123 45 67"
+    println("Telefon: $telefon")
     var posta_kodu = "34000"
+    println("Posta Kodu: $posta_kodu")
     var email = "ahmt123013@gmail.com"
+    println("Email: $email")
     var meslek = "Öğrenci"
+    println("Meslek: $meslek")
     var stok_miktari = 100
+    println("Stok Miktarı: $stok_miktari")
     var musteri_adi = "Ahmet Demir"
+    println("Müşteri Adı: $musteri_adi")
     var bakiye = 1000.0
+    println("Bakiye: $bakiye")
     var dogum_gunu = "01.01.1998"
+    println("Doğum Günü: $dogum_gunu")
     var maas = 22400
+    println("Maaş: $maas")
     var medeni_durum = "Bekar"
+    println("Medeni Durum: $medeni_durum")
     var urun_yorumu = "Ürün çok güzel"
+    println("Ürün Yorumu: $urun_yorumu")
     var odeme_tarihi = "01.01.2023"
+    println("Ödeme Tarihi: $odeme_tarihi")
     var odeme = true
+    println("Ödeme: $odeme")
     var siparis_adedi = 5
+    println("Sipariş Adedi: $siparis_adedi")
     var araba_modeli = "BMW"
+    println("Araba Modeli: $araba_modeli")
     var kitap_ismi = "Küçük Prens"
+    println("Kitap İsmi: $kitap_ismi")
     var yayinlanma_tarihi = "01.01.2020"
+    println("Yayınlanma Tarihi: $yayinlanma_tarihi")
     var indirim_miktari = 0.2
+    println("İndirim Miktarı: $indirim_miktari")
     var oda_sayisi:Byte = 3
+    println("Oda Sayısı: $oda_sayisi")
     var enlem:Float = 39.92f
+    println("Enlem: $enlem")
     var boylam:Double = 32.85
+    println("Boylam: $boylam")
     var urun_adi = "Laptop"
+    println("urun_adi: $urun_adi")
     var yemek_fiyati = 50.0
+    println("Yemek Fiyatı: $yemek_fiyati")
     var marka:String = "Apple"
+    println("Marka: $marka")
     var muzik_adi = "Shape of You"
+    println("Muzik Adı: $muzik_adi")
     var video_suresi = 120
+    println("Video Süresi: $video_suresi")
     var urun_puani:Byte = 5
+    println("Ürün Puanı: $urun_puani")
     var resim_adı = "resim.jpg"
+    println("Resim Adı: $resim_adı")
     var dosya_turu = "jpg"
+    println("Dosya Türü: $dosya_turu")
     var renk= "Kırmızı"
+    println("Renk: $renk")
     var renk_kodu = "#FF0000"
+    println("Renk Kodu: $renk_kodu")
     var telefon_modeli = "iPhone"
+    println("Telefon Modeli: $telefon_modeli")
     var ekran_boyutu = 6.1
+    println("Ekran Boyutu: $ekran_boyutu")
     var agirlik = 1.5
+    println("Ağırlık: $agirlik")
     var ulusal_gun:String = "29 Ekim"
+    println("Ulusal Gün: $ulusal_gun")
     var tatil_gunleri = 10
+    println("Tatil Günleri: $tatil_gunleri")
     var rezervasyon_tarihi = "01.01.2023"
+    println("Rezervasyon Tarihi: $rezervasyon_tarihi")
     var sokak_adi = "Atatürk Caddesi"
+    println("Sokak Adı: $sokak_adi")
     var otobus_hatti = 34
+    println("Otobüs Hattı: $otobus_hatti")
     var kalan_dakika = 30
+    println("Kalan Dakika: $kalan_dakika")
     var takip_kodu = "123456789"
+    println("Takip Kodu: $takip_kodu")
     var kupon_suresi = "01.01.2023"
+    println("Kupon Süresi: $kupon_suresi")
     var kupon_kodu = "KAMPANYA2023"
+    println("Kupon Kodu: $kupon_kodu")
     var fatura_adresi = "Merkez Mahallesi No:1 Daire:1 Istanbul Türkiye"
+    println("Fatura Adresi: $fatura_adresi")
 
 
 }
